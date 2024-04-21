251ee4f982beec8adee49dc54c9bd2ef0f6d145a
Ashkar_Noorul <noorulashkar007@gmail.com>
2024-04-22 00:10:54 +0530

    added product details page except modal

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,8 +1,9 @@
 "use server";
-
+import { revalidatePath } from "next/cache";
 import Product from "../models/productmodel";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if (!productUrl) return;
@@ -11,6 +12,30 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     connectToDB();
 
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+    if (!scrapedProduct) return;
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+    revalidatePath(`/products/${newProduct._id}`);
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`);
   }
@@ -20,6 +45,32 @@ export async function getAllProducts() {
     connectToDB();
     const products = await Product.find();
     return products;
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+    const currentProduct = await Product.findById(productId);
+    if (!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+    const product = await Product.findOne({ id: productId });
+
+    if (!product) return null;
+    return product;
   } catch (error) {
     console.log(error);
   }
--- lib/utils.ts
+++ lib/utils.ts
@@ -42,3 +42,36 @@ export function extractDescription($: any) {
   }
   return "";
 }
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+  for (let i = 1; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+  for (let i = 1; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+  return averagePrice;
+}
+
+export const formatNumber = (num: number = 0) => {
+  return num.toLocaleString(undefined, {
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  });
+};
