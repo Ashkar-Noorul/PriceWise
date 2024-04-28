111a7f7d0a82170d999829f30ecd20e466c41ff7
Ashkar_Noorul <noorulashkar007@gmail.com>
2024-04-28 12:06:32 +0530

    implemented cron job

---
+++ app/api/cron/route.ts
@@ -0,0 +1,86 @@
+import Product from "@/lib/models/productmodel";
+import { connectToDB } from "@/lib/mongoose";
+import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import {
+  getAveragePrice,
+  getEmailNotifyType,
+  getHighestPrice,
+  getLowestPrice,
+} from "@/lib/utils";
+import { NextResponse } from "next/server";
+import { title } from "process";
+
+export async function GET(request: Request) {
+  try {
+    connectToDB();
+    const products = await Product.find({});
+
+    if (!products) throw new Error("No product fetched");
+
+    //1. scrape latest product details & update the pricehistory of the item
+    const updatedProducts = await Promise.all(
+      products.map(async (currentProduct) => {
+        //scrape the product
+        const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
+
+        if (!scrapedProduct) return;
+
+        const updatedPriceHistory = [
+          ...currentProduct.priceHistory,
+          {
+            price: scrapedProduct.currentPrice,
+          },
+        ];
+        const product = {
+          ...scrapedProduct,
+          priceHistory: updatedPriceHistory,
+          lowestPrice: getLowestPrice(updatedPriceHistory),
+          highestPrice: getHighestPrice(updatedPriceHistory),
+          averagePrice: getAveragePrice(updatedPriceHistory),
+        };
+
+        //updating the product in DB
+        const updatedProduct = await Product.findOneAndUpdate(
+          {
+            url: product.url,
+          },
+          product
+        );
+
+        //2. Check each product's status & send email accordingly
+
+        const emailnotifyType = getEmailNotifyType(
+          scrapedProduct,
+          currentProduct
+        );
+
+        if (emailnotifyType && updatedProduct.users.length > 0) {
+          const productInfo = {
+            title: updatedProduct.title,
+            url: updatedProduct.url,
+          };
+
+          const emailContent = await generateEmailBody(
+            productInfo,
+            emailnotifyType
+          );
+
+          const userEmails = updatedProduct.users.map(
+            (user: any) => user.email
+          );
+
+          await sendEmail(emailContent, userEmails);
+        }
+        return updatedProduct;
+      })
+    );
+
+    return NextResponse.json({
+      message: "Ok",
+      data: updatedProducts,
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to get all products: ${error.message}`);
+  }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,12 @@
 import { PriceHistoryItem, Product } from "@/types";
 
+const Notification = {
+  WELCOME: "WELCOME",
+  CHANGE_OF_STOCK: "CHANGE_OF_STOCK",
+  LOWEST_PRICE: "LOWEST_PRICE",
+  THRESHOLD_MET: "THRESHOLD_MET",
+};
+
 const THRESHOLD_PERCENTAGE = 40;
 
 //extracts and returns the price from a list of possible elements.
@@ -68,6 +75,25 @@ export function getAveragePrice(priceList: PriceHistoryItem[]) {
   const averagePrice = sumOfPrices / priceList.length || 0;
   return averagePrice;
 }
+
+export const getEmailNotifyType = (
+  scrapedProduct: Product,
+  currentProduct: Product
+) => {
+  const lowestPrice = getLowestPrice(currentProduct.priceHistory);
+
+  if (scrapedProduct.currentPrice < lowestPrice) {
+    return Notification.LOWEST_PRICE as keyof typeof Notification;
+  }
+  if (!scrapedProduct.isOutOfStock && currentProduct.isOutOfStock) {
+    return Notification.CHANGE_OF_STOCK as keyof typeof Notification;
+  }
+  if (scrapedProduct.discountRate >= THRESHOLD_PERCENTAGE) {
+    return Notification.THRESHOLD_MET as keyof typeof Notification;
+  }
+
+  return null;
+};
 
 export const formatNumber = (num: number = 0) => {
   return num.toLocaleString(undefined, {
